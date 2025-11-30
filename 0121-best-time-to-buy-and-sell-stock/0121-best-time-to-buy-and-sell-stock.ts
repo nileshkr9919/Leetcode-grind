176136fadd4f2c3a91bb1b5cacc8d51ed09bf32d
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2025-11-30 13:45:07 +0530

    Time: 10 ms (7.6%), Space: 67 MB (14.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,10 +1,24 @@
 function maxProfit(prices: number[]): number {
-    let maxProfit = 0, minPrice = prices[0];
+    //Approach 1: Bruteforce
+    // let maxProfit = 0, minPrice = prices[0];
 
-    for (let i = 1; i < prices.length; i++) {
-        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    // for (let i = 1; i < prices.length; i++) {
+    //     maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    //     minPrice = Math.min(prices[i], minPrice);
+    // }
+
+    // return maxProfit;
+
+    // Approach 2: DP (Bottom-up/ Tabulation)
+    const n = prices.length;
+    const dp = new Array(n).fill(0);
+
+    let minPrice = prices[0];
+
+    for (let i = 1; i < n; i++) {
+        dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
         minPrice = Math.min(prices[i], minPrice);
     }
 
-    return maxProfit;
+    return dp[n - 1];
 };
