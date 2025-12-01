b5be346218dda846de7756f20e1e1328217f129c
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2025-12-01 13:23:39 +0530

    Time: 1 ms (56.34%), Space: 61.9 MB (72.96%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -0,0 +1,13 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+};
