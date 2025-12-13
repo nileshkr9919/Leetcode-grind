5b0c20a4f39f9da43d1f2df1aecdffb640eb799a
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2025-12-13 23:59:28 +0530

    Time: 10 ms (40%), Space: 66.5 MB (37.85%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
@@ -0,0 +1,20 @@
+function findDuplicate(nums: number[]): number {
+    let i = 0;
+    while (i < nums.length) {
+        let pos = nums[i] -1;
+
+        if (nums[i] !== nums[pos]) {
+            [nums[i], nums[pos]] = [nums[pos], nums[i]];
+        } else {
+            i++;
+        }
+    }
+
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] !== i+1) {
+            return nums[i];
+        }
+    }
+
+    return nums.length;
+};
