47369d1ecaea8e9697d2171c6abbaf8484d1bc6b
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2025-12-20 00:08:21 +0530

    Time: 22 ms (55.53%), Space: 48.8 MB (69.94%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,22 +1,15 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         List<List<String>> result = new ArrayList<>();
-        List<String> sortedProducts = new ArrayList<>();
+        Arrays.sort(products);
+        String queryString = searchWord.toLowerCase();
 
-        for (int i = 0; i < products.length; i++) {
-            String product = products[i].toLowerCase();
-            if (product.startsWith(searchWord.substring(0,1))) {
-                sortedProducts.add(product);
-            }
-        }
-        Collections.sort(sortedProducts);
-
-        for (int i = 1; i <= searchWord.length(); i++) {
-            String query = searchWord.substring(0, i);
+        for (int i = 1; i <= queryString.length(); i++) {
+            String query = queryString.substring(0, i);
             List<String> resultForTempQuery = new ArrayList<>();
 
-            for (String product : sortedProducts) {
-                if (product.startsWith(query.toLowerCase()) && resultForTempQuery.size() < 3) {
+            for (String product : products) {
+                if (product.startsWith(query) && resultForTempQuery.size() < 3) {
                     resultForTempQuery.add(product);
                 }
             }
