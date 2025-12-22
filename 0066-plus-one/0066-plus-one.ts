03b7a5b32373c8129cd9b9359f314d2518cd7e4f
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2025-12-22 17:51:28 +0530

    0066-plus-one

---
+++ 0066-plus-one/0066-plus-one.ts
@@ -0,0 +1,19 @@
+function plusOne(digits: number[]): number[] {
+    let index = digits.length - 1, carry = 0;
+
+    while(index >= 0) {
+        digits[index]++;
+
+        if(digits[index] === 10) {
+            digits[index] = 0;
+        } else {
+            break;
+        }
+
+        index--;
+    }
+
+    if(digits[0] === 0) digits.unshift(1);
+
+    return digits;
+};
