357ba83c726b659bcdd1300d830bfabe2dd06dfb
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2025-12-28 02:00:18 +0530

    2402-meeting-rooms-iii

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -1,43 +1,46 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
-        Arrays.sort(meetings, (a,b) -> a[0] - b[0]);
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
 
         PriorityQueue<Integer> freeRooms = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             freeRooms.offer(i);
-        }
 
-        PriorityQueue<long[]> busyRooms = new PriorityQueue<>((a, b) -> a[0] == b[0] ? Long.compare(a[1], b[1]) : Long.compare(a[0], b[0]));
+        PriorityQueue<long[]> busyRooms = new PriorityQueue<>(
+                (a, b) -> a[0] == b[0] ? Long.compare(a[1], b[1]) : Long.compare(a[0], b[0]));
 
         int[] count = new int[n];
 
         for (int[] meeting : meetings) {
             long start = meeting[0];
             long end = meeting[1];
-            long duration = end - start;
 
-            // Free rooms that have finished
+            long diff = end - start;
+
             while (!busyRooms.isEmpty() && busyRooms.peek()[0] <= start) {
-                freeRooms.offer((int) busyRooms.poll()[1]);
+                freeRooms.offer((int)busyRooms.poll()[1]);
             }
 
             if (!freeRooms.isEmpty()) {
                 int room = freeRooms.poll();
-                busyRooms.offer(new long[]{end, room});
+                busyRooms.offer(new long[] { end, room });
                 count[room]++;
             } else {
                 long[] earliest = busyRooms.poll();
-                long newEnd = earliest[0] + duration;
-                int room = (int) earliest[1];
-                busyRooms.offer(new long[]{newEnd, room});
-                count[room]++;
+                long room = earliest[1];
+                long newEnd = earliest[0] + diff;
+                busyRooms.offer(new long[] { newEnd, room });
+                count[(int)room]++;
             }
         }
 
         int res = 0;
-        for (int i = 1; i < n; i++) {
-            if (count[i] > count[res]) res = i;
+        for (int i = 0; i < n; i++) {
+            if (count[i] > count[res]) {
+                res = i;
+            }
         }
+
         return res;
     }
 }
