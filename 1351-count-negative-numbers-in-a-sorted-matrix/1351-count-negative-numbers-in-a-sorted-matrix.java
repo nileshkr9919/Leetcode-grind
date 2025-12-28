4022ad5b048da5d2ac6ad755bc604e139c4fc0bf
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2025-12-28 22:37:08 +0530

    1351-count-negative-numbers-in-a-sorted-matrix

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int res = 0;
+
+        for(int i=0;i<grid.length;i++) {
+            for(int j=grid[i].length - 1;j>=0;j--) {
+                if(grid[i][j] < 0) {
+                    res++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
