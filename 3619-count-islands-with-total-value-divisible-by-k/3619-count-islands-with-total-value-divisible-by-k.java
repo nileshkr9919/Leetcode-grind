b12598b13b3aa8f6e01534e2ca3c74b95b477160
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-03 00:49:50 +0530

    3619-count-islands-with-total-value-divisible-by-k

---
+++ 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int countIslands(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length, count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 0) {
+                    long valueOfIsland = bfs(grid, i, j);
+                    if (valueOfIsland % k == 0)
+                        count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    // DFS Approach
+    // private int dfs(char[][] grid, int i, int j) {
+    //     if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+    //         return;
+    //     }
+
+    //     grid[i][j] = '0';
+
+    //     dfs(grid, i, j + 1);
+    //     dfs(grid, i, j - 1);
+    //     dfs(grid, i - 1, j);
+    //     dfs(grid, i + 1, j);
+
+    //     return;
+    // }
+
+    // BFS Approach
+    private long bfs(int[][] grid, int idx, int jdx) {
+        Deque<int[]> queue = new LinkedList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+
+        long sumOfIsland = grid[idx][jdx];
+
+        int[] rowDir = { -1, 0, 0, 1 };
+        int[] colDir = { 0, -1, 1, 0 };
+
+        queue.add(new int[] { idx, jdx });
+        grid[idx][jdx] = 0;
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int i = cell[0];
+            int j = cell[1];
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nr = i + rowDir[dir];
+                int nc = j + colDir[dir];
+
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] != 0) {
+                    sumOfIsland += grid[nr][nc];
+                    grid[nr][nc] = 0;
+                    queue.add(new int[] { nr, nc });
+                }
+            }
+        }
+        System.out.println(sumOfIsland);
+        return sumOfIsland;
+    }
+}
