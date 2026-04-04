0a1c593b926e0d0013614f6aa0d235d4fadbbe61
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-04 22:25:13 +0530

    0200-number-of-islands

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,12 +1,14 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length, n = grid[0].length, count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    bfs(grid, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
@@ -14,48 +16,16 @@ class Solution {
         return count;
     }
 
-    // DFS Approach
-    // private void dfs(char[][] grid, int i, int j) {
-    //     if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
-    //         return;
-    //     }
-
-    //     grid[i][j] = '0';
-
-    //     dfs(grid, i, j + 1);
-    //     dfs(grid, i, j - 1);
-    //     dfs(grid, i - 1, j);
-    //     dfs(grid, i + 1, j);
-
-    //     return;
-    // }
-
-    // BFS Approach
-    private void bfs(char[][] grid, int idx, int jdx) {
-        Deque<int[]> queue = new LinkedList<>();
-        int m = grid.length;
-        int n = grid[0].length;
-
-        int[] rowDir = { -1, 0, 0, 1};
-        int[] colDir = { 0, -1, 1, 0};
-
-        queue.add(new int[] { idx, jdx });
-
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            int i = cell[0];
-            int j = cell[1];
-
-            for (int dir = 0; dir < 4; dir++) {
-                int nr = i + rowDir[dir];
-                int nc = j + colDir[dir];
-
-                if (nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1') {
-                    grid[nr][nc] = '0';
-                    queue.add(new int[] { nr, nc });
-                }
-            }
+    //DFS APProach
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
         }
-        grid[idx][jdx] = '0';
+
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 }
