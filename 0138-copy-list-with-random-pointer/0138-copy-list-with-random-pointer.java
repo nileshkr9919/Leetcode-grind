a5e1d8e09fc11c0c0016dfaae6aad7f313821063
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-04 23:25:27 +0530

    0138-copy-list-with-random-pointer

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,50 +15,34 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null)
-            return null;
         HashMap<Node, Node> map = new HashMap<>();
-        bfs(head, map);
+
+        dfs(head, map);
+
         return map.get(head);
     }
 
-    // DFS Approach
-    // private void dfs(Node node, HashMap<Node, Node> map) {
-    //     if (node == null)
-    //         return;
-    //     map.put(node, new Node(node.val));
-
-    //     for (Node neighbour : node.neighbors) {
-    //         if (!map.containsKey(neighbour)) {
-    //             dfs(neighbour, map);
-    //         }
-    //         map.get(node).neighbors.add(map.get(neighbour));
-    //     }
-    // }
-
-    // BFS Approach
-    private void bfs(Node node, HashMap<Node, Node> map) {
-        Deque<Node> queue = new LinkedList<>();
-        map.put(node, new Node(node.val));
-        queue.add(node);
-
-        while (!queue.isEmpty()) {
-            Node curr = queue.poll();
-            if (curr == null)
-                return;
-
-            if (curr.next != null && !map.containsKey(curr.next)) {
-                map.put(curr.next, new Node(curr.next.val));
-                queue.add(curr.next);
-            }
-
-            if (curr.random != null && !map.containsKey(curr.random)) {
-                map.put(curr.random, new Node(curr.random.val));
-                queue.add(curr.random);
-            }
-            map.get(curr).next = map.get(curr.next);
-            map.get(curr).random = map.get(curr.random);
+    private void dfs(Node node, HashMap<Node, Node> map) {
+        if (node == null) {
+            return;
         }
 
+        Node copy = new Node(node.val);
+        map.put(node, copy);
+
+        if (node.next != null) {
+            if (!map.containsKey(node.next)) {
+                dfs(node.next, map);
+            }
+        }
+
+        if (node.random != null) {
+            if (!map.containsKey(node.random)) {
+                dfs(node.random, map);
+            }
+        }
+
+        copy.next = map.get(node.next);
+        copy.random = map.get(node.random);
     }
 }
