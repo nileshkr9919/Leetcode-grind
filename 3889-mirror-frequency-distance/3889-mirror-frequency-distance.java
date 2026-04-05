e369fcac3f34d33d9ac9f3292d8dc888af544937
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-06 02:18:49 +0530

    3889-mirror-frequency-distance

--- 3889-mirror-frequency-distance/3889-mirror-frequency-distance.java
+++ 3889-mirror-frequency-distance/3889-mirror-frequency-distance.java
@@ -3,36 +3,35 @@ class Solution {
         char[] letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
         char[] digits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        int sum = 0;
-        HashMap<Character, Integer> freqMap = new HashMap<>();
-        HashMap<Character, Character> mirrorMap = new HashMap<>();
 
+        int sum = 0;
+
+        int[] freq = new int[128];
+        boolean[] visited = new boolean[128];
+
+        // frequency count
         for (char ch : s.toCharArray()) {
-            char m = '\0';
-            if (Character.isLetter(ch)) {
-                m = letters[25 - (ch - 97)];
-            } else if (Character.isDigit(ch)) {
-                m = digits[9 - (ch - 48)];
-            }
-            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
-            mirrorMap.put(ch, m);
+            freq[ch]++;
         }
 
-        HashSet<Character> visited = new HashSet<>();
+        for (char ch : s.toCharArray()) {
+            if (visited[ch]) continue;
 
-        for (Map.Entry<Character, Character> entry : mirrorMap.entrySet()) {
-            char ch = entry.getKey();
-            char mirror = entry.getValue();
+            char mirror = '\0';
 
-            if (visited.contains(ch)) continue;
+            if (Character.isLetter(ch)) {
+                mirror = letters[25 - (ch - 97)];
+            } else if (Character.isDigit(ch)) {
+                mirror = digits[9 - (ch - 48)];
+            }
 
-            int f1 = freqMap.getOrDefault(ch, 0);
-            int f2 = freqMap.getOrDefault(mirror, 0);
+            int f1 = freq[ch];
+            int f2 = freq[mirror];
 
             sum += Math.abs(f1 - f2);
 
-            visited.add(ch);
-            visited.add(mirror);
+            visited[ch] = true;
+            visited[mirror] = true;
         }
 
         return sum;
