105c3e1e3148e0f6d6f4a8e3c7190ffa8c5a134d
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-06 20:51:23 +0530

    0207-course-schedule

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < prerequisites.length; i++) {
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+
+            if (!map.containsKey(b)) {
+                map.put(b, new ArrayList<>());
+            }
+            map.get(b).add(a);
+        }
+
+        System.out.println(map);
+
+        int[] state = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i, map, state)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean dfs(int course, HashMap<Integer, List<Integer>> graph, int[] state) {
+        if(state[course] == 1) return true;
+        if(state[course] == 2) return false;
+
+        state[course] = 1;
+
+        if (graph.containsKey(course)) {
+            List<Integer> neighbors = graph.get(course);
+            for (int neighbor : neighbors) {
+                if (dfs(neighbor, graph, state)) {
+                    return true;
+                }
+            }
+        }
+
+        state[course] = 2;
+        return false;
+    }
+}
