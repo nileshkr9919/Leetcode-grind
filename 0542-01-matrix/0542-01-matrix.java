3d8fd5346520708be421b7031bb9bb5f7319706d
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-12 05:01:27 +0530

    0542-01-matrix

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -3,7 +3,7 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
-        Deque<int[]> queue = new LinkedList<>();
+        Deque<int[]> queue = new ArrayDeque<>();
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -15,30 +15,28 @@ class Solution {
             }
         }
 
-        int[] rowDir = new int[] { 0, 0, 1, -1 };
-        int[] colDir = new int[] { -1, 1, 0, 0 };
+        int[] rDir = new int[] { 0, 0, -1, 1 };
+        int[] cDir = new int[] { -1, 1, 0, 0 };
 
         while (!queue.isEmpty()) {
             int size = queue.size();
 
-            for (int i = 0; i < size; i++) {
-                int[] curr = queue.poll();
+            for (int k = 0; k < size; k++) {
+                int[] cell = queue.poll();
+                int i = cell[0];
+                int j = cell[1];
 
-                int r = curr[0];
-                int c = curr[1];
+                for (int dir = 0; dir < 4; dir++) {
+                    int nr = i + rDir[dir];
+                    int nc = j + cDir[dir];
 
-                for (int d = 0; d < 4; d++) {
-                    int nr = r + rowDir[d];
-                    int nc = c + colDir[d];
-
-                    if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
-                        if (mat[nr][nc] > mat[r][c] + 1) {
-                            mat[nr][nc] = mat[r][c] + 1;
-                            queue.add(new int[] { nr, nc });
+                    if (nr >= 0 && nc >= 0 && nr < m && nc < n) {
+                        if(mat[nr][nc] > mat[i][j] + 1) {
+                            mat[nr][nc] = mat[i][j] + 1;
+                            queue.add(new int[]{nr,nc}); 
                         }
                     }
                 }
-
             }
 
         }
