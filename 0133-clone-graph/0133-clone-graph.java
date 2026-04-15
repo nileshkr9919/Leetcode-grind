4954eac7774f7281c0a14070a8f5a3492d2b3028
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-15 23:35:39 +0530

    0133-clone-graph

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -22,50 +22,22 @@ class Solution {
     public Node cloneGraph(Node node) {
         HashMap<Node, Node> map = new HashMap<>();
 
-        bfs(node, map);
+        dfs(node, map);
 
         return map.get(node);
     }
 
-    // private void dfs(Node node, HashMap<Node, Node> map) {
-    //     if (node == null || map.containsKey(node)) {
-    //         return;
-    //     }
-
-    //     Node copy = new Node(node.val);
-    //     map.put(node, copy);
-
-    //     for (Node nei : node.neighbors) {
-    //         if (!map.containsKey(nei)) {
-    //             dfs(nei, map);
-    //         }
-    //         map.get(node).neighbors.add(map.get(nei));
-    //     }
-    // }
-
-    private void bfs(Node node, HashMap<Node, Node> map) {
-        if (node == null) {
+    private void dfs(Node node, HashMap<Node, Node> map) {
+        if (node == null)
             return;
-        }
 
-        Deque<Node> queue = new LinkedList<>();
+        map.put(node, new Node(node.val));
 
-        Node copy = new Node(node.val);
-        map.put(node, copy);
-
-        queue.add(node);
-
-        while (!queue.isEmpty()) {
-            Node curr = queue.poll();
-
-            for (Node nei : curr.neighbors) {
-                if (!map.containsKey(nei)) {
-                    Node neicopy = new Node(nei.val);
-                    map.put(nei, neicopy);
-                    queue.add(nei);
-                }
-                map.get(curr).neighbors.add(map.get(nei));
+        for (Node nei : node.neighbors) {
+            if (!map.containsKey(nei)) {
+                dfs(nei, map);
             }
+            map.get(node).neighbors.add(map.get(nei));
         }
     }
 }
