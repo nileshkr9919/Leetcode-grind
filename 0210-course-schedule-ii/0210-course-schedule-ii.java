f404cc97481e80b99e610d0e48a3ba6f61a54b7a
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-19 22:21:34 +0530

    0210-course-schedule-ii

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -6,48 +6,59 @@ class Solution {
             graph.add(new ArrayList<>());
         }
 
+        int[] indegree = new int[numCourses];
+
         for (int[] preq : prerequisites) {
             int u = preq[0];
             int v = preq[1];
 
             graph.get(v).add(u);
+            indegree[u]++;
         }
 
-        int[] state = new int[numCourses];
-        List<Integer> res = new ArrayList<>();
+        Deque<Integer> queue = new ArrayDeque<>();
 
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(graph, i, state, res)) {
-                return new int[] {};
+            if (indegree[i] == 0) {
+                queue.add(i);
             }
         }
 
-        Collections.reverse(res);
+        List<Integer> result = new ArrayList<>();
 
-        int[] ans = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            ans[i] = res.get(i);
-        }
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            result.add(node);
 
-        return ans;
-    }
-
-    private boolean dfs(List<List<Integer>> graph, int course, int[] state, List<Integer> res) {
-        if (state[course] == 2) {
-            return true;
-        }
-        if (state[course] == 1) {
-            return false;
-        }
-        state[course] = 1;
-
-        for (int nei : graph.get(course)) {
-            if (!dfs(graph, nei, state, res)) {
-                return false;
+            for (int nei : graph.get(node)) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    queue.add(nei);
+                }
             }
         }
-        state[course] = 2;
-        res.add(course);
-        return true;
+
+        return result.size() == numCourses ? result.stream()
+                .mapToInt(Integer::intValue)
+                .toArray() : new int[]{};
     }
+
+    // private boolean dfs(List<List<Integer>> graph, int course, int[] state, List<Integer> res) {
+    //     if (state[course] == 2) {
+    //         return true;
+    //     }
+    //     if (state[course] == 1) {
+    //         return false;
+    //     }
+    //     state[course] = 1;
+
+    //     for (int nei : graph.get(course)) {
+    //         if (!dfs(graph, nei, state, res)) {
+    //             return false;
+    //         }
+    //     }
+    //     state[course] = 2;
+    //     res.add(course);
+    //     return true;
+    // }
 }
