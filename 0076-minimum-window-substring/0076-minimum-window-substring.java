4145cd14a3ea2275416f3d25205b15277dd2e100
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-20 22:48:17 +0530

    0076-minimum-window-substring

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -8,35 +8,34 @@ class Solution {
 
         HashMap<Character, Integer> sFreq = new HashMap<>();
         int have = 0, need = tFreq.size();
+        int[] res = {-1,-1};
+        int minRes = Integer.MAX_VALUE;
         int left = 0;
-        int[] res = { -1, -1 }; // result window indices
-        int resLen = Integer.MAX_VALUE;
 
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            sFreq.put(c, sFreq.getOrDefault(c, 0) + 1);
+        for(int right=0;right<s.length();right++) {
+            char ch = s.charAt(right);
+            sFreq.put(ch, sFreq.getOrDefault(ch, 0) + 1);
 
-            // did this char satisfy a requirement?
-            if (tFreq.containsKey(c) && sFreq.get(c).equals(tFreq.get(c)))
+            if(tFreq.containsKey(ch) && sFreq.get(ch).equals(tFreq.get(ch))) {
                 have++;
+            }
 
-            // shrink from left while window is valid
-            while (have == need) {
-                // record if smallest window
-                if (right - left + 1 < resLen) {
-                    resLen = right - left + 1;
+            while(have == need) {
+                if(right - left + 1 < minRes) {
+                    minRes = right - left + 1;
                     res[0] = left;
                     res[1] = right;
                 }
-                // shrink
                 char l = s.charAt(left);
                 sFreq.put(l, sFreq.get(l) - 1);
-                if (tFreq.containsKey(l) && sFreq.get(l) < tFreq.get(l))
+                if( tFreq.containsKey(l) && sFreq.get(l) < tFreq.get(l)) {
                     have--;
+                } 
                 left++;
             }
         }
 
         return res[0] == -1 ? "" : s.substring(res[0], res[1] + 1);
+
     }
 }
