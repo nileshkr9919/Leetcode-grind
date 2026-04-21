51dd75519ccfe1eee899b62e7bbb5b3f0a35c698
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-22 01:21:06 +0530

    0076-minimum-window-substring

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,41 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
         HashMap<Character, Integer> tFreq = new HashMap<>();
-
         for (char ch : t.toCharArray()) {
             tFreq.put(ch, tFreq.getOrDefault(ch, 0) + 1);
         }
 
+        int need = tFreq.size(), have = 0, left = 0;
+        int[] res = new int[] { -1, -1 };
+        int minLen = Integer.MAX_VALUE;
         HashMap<Character, Integer> sFreq = new HashMap<>();
-        int have = 0, need = tFreq.size();
-        int[] res = {-1,-1};
-        int minRes = Integer.MAX_VALUE;
-        int left = 0;
 
-        for(int right=0;right<s.length();right++) {
+        for (int right = 0; right < s.length(); right++) {
             char ch = s.charAt(right);
             sFreq.put(ch, sFreq.getOrDefault(ch, 0) + 1);
 
-            if(tFreq.containsKey(ch) && sFreq.get(ch).equals(tFreq.get(ch))) {
+            if (tFreq.containsKey(ch) && tFreq.get(ch).equals(sFreq.get(ch))) {
                 have++;
             }
 
-            while(have == need) {
-                if(right - left + 1 < minRes) {
-                    minRes = right - left + 1;
+            while (have == need) {
+                if (right - left + 1 < minLen) {
+                    minLen = Math.min(minLen, right - left + 1);
                     res[0] = left;
                     res[1] = right;
                 }
-                char l = s.charAt(left);
-                sFreq.put(l, sFreq.get(l) - 1);
-                if( tFreq.containsKey(l) && sFreq.get(l) < tFreq.get(l)) {
+                char ch2 = s.charAt(left);
+                sFreq.put(ch2, sFreq.get(ch2) - 1);
+                if (tFreq.containsKey(ch2) && sFreq.get(ch2) < tFreq.get(ch2)) {
                     have--;
-                } 
+                }
                 left++;
             }
         }
 
         return res[0] == -1 ? "" : s.substring(res[0], res[1] + 1);
-
     }
 }
