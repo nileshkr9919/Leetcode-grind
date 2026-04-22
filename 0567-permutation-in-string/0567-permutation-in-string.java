acdd4ec818fd33db2178b0d28305c285a3666f60
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-22 23:47:25 +0530

    0567-permutation-in-string

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -3,21 +3,22 @@ class Solution {
         if (s1.length() > s2.length())
             return false;
 
-        int[] s1Freq = new int[26];
-        int[] s2Freq = new int[26];
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
 
         for (int i = 0; i < s1.length(); i++) {
-            s1Freq[s1.charAt(i) - 'a']++;
-            s2Freq[s2.charAt(i) - 'a']++;
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
         }
 
-        if (Arrays.equals(s1Freq, s2Freq))
+        if (Arrays.equals(freq1, freq2))
             return true;
 
-        for (int right = s1.length(); right < s2.length(); right++) {
-            s2Freq[s2.charAt(right) - 'a']++; // add right
-            s2Freq[s2.charAt(right - s1.length()) - 'a']--; // remove left
-            if (Arrays.equals(s1Freq, s2Freq))
+        for (int i = s1.length(); i < s2.length(); i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            freq2[(s2.charAt(i - s1.length())) - 'a']--;
+
+            if (Arrays.equals(freq1, freq2))
                 return true;
         }
 
