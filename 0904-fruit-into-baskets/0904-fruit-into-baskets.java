664a5fd58a45c0a2874a9b2508035931bccb03c9
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-24 00:15:20 +0530

    0904-fruit-into-baskets

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,27 +1,24 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+        int left = 0, maxLen = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0, maxTotal = 0, total = 0;
 
         for (int right = 0; right < fruits.length; right++) {
             int fruitType = fruits[right];
             map.put(fruitType, map.getOrDefault(fruitType, 0) + 1);
 
             while (map.size() > 2) {
-                int leftFruit = fruits[left];
-                map.put(leftFruit, map.get(leftFruit) - 1);
-
-                if (map.get(leftFruit) == 0) {
-                    map.remove(leftFruit);
+                int leftFruitType = fruits[left];
+                map.put(leftFruitType, map.get(leftFruitType) - 1);
+                if(map.get(leftFruitType) == 0) {
+                    map.remove(leftFruitType);
                 }
-
                 left++;
             }
 
-            maxTotal = Math.max(maxTotal, right - left + 1);
-
+            maxLen = Math.max(maxLen, right - left + 1);
         }
 
-        return maxTotal;
+        return maxLen;
     }
 }
