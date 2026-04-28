bb528fac1286473860b9e9c50b56bcd9837267cf
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-04-28 23:21:50 +0530

    0056-merge-intervals

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        // Sort by start time
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        // Loop over intervals and merge intervals with start time less than end time of prev interval
+
+        List<int[]> res = new ArrayList<>();
+
+        int[] currentInterval = new int[] { intervals[0][0], intervals[0][1] };
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+
+            if (interval[0] <= currentInterval[1] || currentInterval[1] >= interval[1]) {
+                currentInterval[1] = Math.max(interval[1], currentInterval[1]);
+            } else {
+                res.add(currentInterval);
+                currentInterval = interval;
+            }
+        }
+
+        res.add(currentInterval);
+
+        int[][] arr = new int[res.size()][2];
+
+        for (int i = 0; i < res.size(); i++) {
+            arr[i] = res.get(i);
+        }
+
+        return arr;
+    }
+}
